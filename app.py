f24ac62a5f0e47900e929f723afa049fab071e18
almas yaar khan <almasyarkhan007@gmail.com>
2023-08-05 20:40:05 +0530

    login v2 added

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
 from fastapi import FastAPI
 import uvicorn
+from db_manager import db_connect
 
 app = FastAPI()
+db = db_connect()
 
 
-@app.get("/")
-def foo():
-    return True
+@app.get("/login/{username}/{password}")
+def login(username,password):
+    
+    user_detail = db["users"].find({"name":username,"password":password})
+
+
+    if len(list(user_detail)) > 0:
+
+        return "Login succesfull"
+
+    return "Authentication failed"
+
+
+@app.get("/login_v2/{username}/{password}")
+def login_v2(username,password):
+    
+    user_detail = db["users"].find({"name":username})
+
+    user_details = list(user_detail)
+
+    if len(user_details) > 0:
+
+        if user_details[0]['password'] == password:
+            return "Login succesfull"
+
+        return "Incorrect password"
+    
+    return "user not found"
+
+
+
+
 
 if __name__ == "__main__":
-    uvicorn.run(app,port=8000)
+    uvicorn.run("app:app",port=8000,reload=True)
---
+++ db_manager.py
@@ -0,0 +1,9 @@
+import pymongo
+import certifi
+import json
+
+def db_connect():
+
+    db = pymongo.MongoClient("mongodb+srv://almasyarkhan007:1eiygny3nsMA8kM0@cluster1.voubafg.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
+    db = db["fastapi_db"] 
+    return db
